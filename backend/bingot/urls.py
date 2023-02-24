1c247c4b3ca25284cdf46ce0beb307e64774ecc9
조예지 <celpegor216@gmail.com>
2023-02-24 21:51:11 +0900

    #19 🐛 fix: fix root urlpatterns

--- backend/bingot/urls.py
+++ backend/bingot/urls.py
@@ -17,9 +17,9 @@ from django.contrib import admin
 from django.urls import path, include
 
 urlpatterns = [
-    path('admin/', admin.site.urls),
-    path('accounts/', include('accounts.urls')),
-    path('boards/', include('boards.urls')),
-    path('groups/', include('groups.urls')),
-    path('articles/', include('articles.urls')),
+    path('api/admin/', admin.site.urls),
+    path('api/accounts/', include('accounts.urls')),
+    path('api/boards/', include('boards.urls')),
+    path('api/groups/', include('groups.urls')),
+    path('api/articles/', include('articles.urls')),
 ]
