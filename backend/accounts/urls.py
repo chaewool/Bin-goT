99f5502f1f7d6bdfdad91ecb0e4c2bde97956787
조예지 <celpegor216@gmail.com>
2023-02-24 21:52:25 +0900

    #19 ✨ feat: add kakao login view

--- backend/accounts/urls.py
+++ backend/accounts/urls.py
@@ -1,5 +1,8 @@
 from django.urls import path
+from . import views
 
 app_name = 'accounts'
 urlpatterns = [
+    path('kakao/', views.KakaoView.as_view(), name='kakao'),
+    path('kakao/callback/', views.KaKaoCallBackView.as_view(), name='kakaoCallback'),
 ]
