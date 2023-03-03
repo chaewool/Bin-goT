e6706b850c37510aa489679f6590fbc2b30d1dcc
조예지 <celpegor216@gmail.com>
2023-03-03 20:23:46 +0900

    #19 🐛 fix: fix typo

--- backend/accounts/serializers.py
+++ backend/accounts/serializers.py
@@ -4,4 +4,4 @@ from rest_framework import serializers
 class UserSerializer(serializers.ModelSerializer):
     class Meta:
         model = get_user_model()
-        fields = ('id', 'username', 'profile', 'noti_rank', 'noti_due', 'noti_chat', 'is_activate', 'groups')
+        fields = ('id', 'username', 'profile', 'noti_rank', 'noti_due', 'noti_chat', 'is_active', 'groups')
