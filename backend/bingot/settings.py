f469299f4f4bc981f90302d8d75b368e0df16d23
조예지 <celpegor216@gmail.com>
2023-06-01 21:08:16 +0900

    #40 🐛 fix: chang refresh token lifetime

--- backend/bingot/settings.py
+++ backend/bingot/settings.py
@@ -171,7 +171,7 @@ REST_FRAMEWORK = {
 
 SIMPLE_JWT = {
     'ACCESS_TOKEN_LIFETIME': timedelta(seconds=30),
-    'REFRESH_TOKEN_LIFETIME': timedelta(minutes=1),
+    'REFRESH_TOKEN_LIFETIME': timedelta(minutes=10),
     'ROTATE_REFRESH_TOKENS': False,
     'BLACKLIST_AFTER_ROTATION': True,
     'TOKEN_USER_CLASS': 'user.User',
